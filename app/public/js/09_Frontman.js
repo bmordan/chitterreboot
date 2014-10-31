81b1724304d4c7468be4793c5f248bdf76f16690
Bernard Mordan <bernardmordan@gmail.com>
2014-10-31 10:07:48 +0000

    counter working and styled

--- app/public/js/09_Frontman.js
+++ app/public/js/09_Frontman.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
   var id = $('main samp').html()
-
+// If a user is not logged in
   if( id === '' ){
     $('section.preauth').append()
     $('main').hide()
@@ -9,27 +9,31 @@ $(document).ready(function(){
     $('header nav ul li i.fa').hide()
     $('#peep-dialog').hide()
   }else{
+    // grab the data
     $.get('/api/chitter', function(data){
       var peepsTemplate   = Handlebars.compile( $('template.peeps').html()   )
       var profileTemplate = Handlebars.compile( $('template.profile').html() )
       var totalPeeps = 0
-
+      // render the user profile template
       for(var i=0;i<data.length;i+=1){
         if (data[i].user.id === parseInt(id)) {
           var profileHtml = profileTemplate({name: data[i].user.name, handle: data[i].user.handle, totalPeeps: totalPeeps +=1 })
         }
       }
-
+      //populate the user section
       $('main section.peep-profile').append(profileHtml)
-
+      // manage the click and reveal peep input box
       $('#profile-peep').focus( function() {
         $(this).attr('placeholder','').attr('rows','5')
         $('main section artical form').toggleClass('input')
+        $('main section artical form div').show()
         $('main section artical form a.peep-button').show()
         $('main section artical form div').css('height','1.1em')
         $('main section artical form a.peep-button').css('opacity','0.5')
         $(this).on('input', function() {
           $('main section artical form a.peep-button').css('opacity','1')
+          var length = (144 - parseInt($(this).val().length))
+          $('div.peep-counter').text(length)
         })
         $('.peep-button').click(function(){
           var peep = $('#profile-peep').val()
@@ -39,13 +43,12 @@ $(document).ready(function(){
         })
       })
       $('#profile-peep').blur( function() {
-        if($(this).val() === ''){
-          $(this).attr('placeholder','compose a new peep').attr('rows','1')
-          $('main section artical form').toggleClass('input')
-          $('main section artical form div').css('height','0em')
-          $('main section artical form a.peep-button').hide()
-        }
-        
+        $(this).attr('placeholder','compose a new peep').attr('rows','1')
+        $('main section artical form').toggleClass('input')
+        $('main section artical form div').css('height','0em')
+        $('main section artical form a.peep-button').hide()
+        $('main section artical form div.peep-counter').hide()
+        $('main section artical form textarea').val('')  
       })
 
       for(var i=0;i<data.length;i+=1){
@@ -55,25 +58,31 @@ $(document).ready(function(){
     })
     $('section.preauth').remove()
     $('main').show()
+    $('main div.peep-counter').hide()
     $('header nav ul li a.peep-button').show()
     $('header nav ul li a.peep-button').click(function(){
+      $('#peep-dialog textarea').val('')
+      $('#peep-dialog footer div.peep-counter').text(144)
       $('#peep-dialog').show()
+      $('#peep-dialog textarea').on('input', function() {
+        var length = (144 - parseInt($(this).val().length))
+        $('#peep-dialog footer div.peep-counter').text(length)
+      })
       $('#pop-peep-button').click(function(){
         var peep = $('div#peep-dialog textarea').val();
-        //var id = $('main samp').html();
         console.log(peep,id)
         $.post('/api/chitter',{peep: peep,user_id: id},function(){
           window.location.assign('/')
         })
       })
       $('#peep-dialog').dialog({ 
-          minWidth: 640 ,
-          show: { effect: "blind", duration: 800 },
-          open: function(event,ui) {
-            $('.ui-dialog-titlebar-close')
-            .removeClass("ui-dialog-titlebar-close")
-            .html('<span style="float:right;">X</span>');
-          }
+        minWidth: 640 ,
+        show: { effect: "blind", duration: 800 },
+        open: function(event,ui) {
+          $('.ui-dialog-titlebar-close')
+          .removeClass("ui-dialog-titlebar-close")
+          .html('<span style="float:right;">X</span>');
+        }
       })
     })
 
